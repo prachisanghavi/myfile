dead7d0e9d62d23f5b9ee16bd2c3ad6ca25a10ea
prachisanghavi <70756624+prachisanghavi@users.noreply.github.com>
2021-06-01 15:10:54 +0530

    day2

---
+++ day2/switch_casedemo.php
@@ -0,0 +1,27 @@
+<?php
+$a=7;
+switch ($a){
+    case 1:
+        echo "monday";
+        break;
+    case 2:
+        echo"thusday";
+        break;
+    case 3:
+        echo"wednesday";
+        break;
+    case 4:
+        echo "thruseday";
+        break;
+    case 5:
+        echo "friday";
+        break;
+    case 6:
+        echo "saturday";
+        break;
+    case 7:
+        echo "sunday";
+        break;
+    default :
+        echo "please select 1-7";
+}
