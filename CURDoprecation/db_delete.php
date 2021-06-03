0ec788f1c151ea9a1e37cf3298edc73aca9f9c1b
prachisanghavi <70756624+prachisanghavi@users.noreply.github.com>
2021-06-03 20:44:42 +0530

    Add files via upload

---
+++ CURDoprecation/db_delete.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$username = "root";
+$passwd = "";
+$dbname = "db_intership";
+
+$connection = mysqli_connect($host, $username, $passwd, $dbname);
+
+$id = $_GET['deleteid'];
+
+$q = mysqli_query(
+	$connection,
+	"update tbl_user set is_delete = 1 where user_id='{$id}'"
+) or die("Error" . mysqli_error($connection));
+
+if ($q) {
+	echo "<script>alert('Record Deleted');window.location='db_display.php';</script>";
+}
